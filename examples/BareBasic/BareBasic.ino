005bbfb2f2f39f7cd575b52b8d135a5c73df2707
Robert Ferney <capnregex@gmail.com>
2016-04-22 17:24:10 -0600

    added bare basic

---
+++ examples/BareBasic/BareBasic.ino
@@ -0,0 +1,21 @@
+
+#define RE 7
+#define RA 10
+#define RB 9
+#define LE 8
+#define LA 2
+#define LB 3
+
+void setup() {
+  pinMode(RE, OUTPUT);
+  pinMode(RA, OUTPUT);
+  pinMode(RB, OUTPUT);
+  pinMode(LE, OUTPUT);
+  pinMode(LA, OUTPUT);
+  pinMode(LB, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
